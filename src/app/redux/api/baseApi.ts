37bb2bc7de954f152d9d673f293a849d4088955d
Ekramul <mdekramulhassan168@gmail.com>
2024-08-15 20:42:11 +0600

    api added

--- src/app/redux/api/baseApi.ts
+++ src/app/redux/api/baseApi.ts
@@ -16,8 +16,8 @@ interface ErrorResponse {
 }
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/v1",
-  // baseUrl: "https://medicine-e-commerce-server-green.vercel.app/api/v1",
+  // baseUrl: "http://localhost:5000/api/v1",
+  baseUrl: "https://medicine-e-commerce-server-green.vercel.app/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;

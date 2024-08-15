e8f6a3df4b69cf271837b6fe3d177875b1a1bd0b
Ekramul <mdekramulhassan168@gmail.com>
2024-08-15 11:48:04 +0600

    all error solve

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -12,14 +12,16 @@ const Profile = () => {
       <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
         <div className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50 ">
           <button className="w-9 h-9 md:w-14 md:h-14 object-cover rounded-full overflow-hidden focus:outline-none">
-            <Image
-              src={user?.imageUrl}
-              alt="Profile Image"
-              layout="responsive"
-              width={40}
-              height={40}
-              className="w-full h-full rounded-full object-cover object-center"
-            />
+            {user?.imageUrl && (
+              <Image
+                src={user.imageUrl}
+                alt="Profile Image"
+                layout="responsive"
+                width={40}
+                height={40}
+                className="w-full h-full rounded-full object-cover object-center"
+              />
+            )}
           </button>
 
           <div>
